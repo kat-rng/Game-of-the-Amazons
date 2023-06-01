c5667cc577ec539ea34ae153c09b18a74d76e1f2
kat-rng <92893261+kat-rng@users.noreply.github.com>
2023-06-01 17:51:43 -0400

    Creating the game loop
    Added a run boolean variable that determines whether the loop should continue
    
    Added a loop with a time delay for running the game

--- main.py
+++ main.py
@@ -10,3 +10,10 @@ window = pygame.display.set_mode((500, 500))
 
 # Adding a title to the window
 pygame.display.set_caption("Practice game")
+
+# Main game loop
+run = True
+while run:
+    # Divide 1000 by the number below to get FPS estimate
+    pygame.time.delay(100)
+
