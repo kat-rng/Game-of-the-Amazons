ccab2b6885319036dc16213ed176c14745b62545
kat-rng <92893261+kat-rng@users.noreply.github.com>
2023-06-01 18:12:17 -0400

    Added code to display a rectangle
    Added the variables to define where a rectangle should appear and what it should look like.
    
    Added code to display a rectangle on screen (pygame.draw.rect)

--- main.py
+++ main.py
@@ -10,6 +10,12 @@ window = pygame.display.set_mode((500, 500))
 # Adding a title to the window
 pygame.display.set_caption("Practice game")
 
+# Setting variables for displaying a rectangle
+x = 50
+y = 50
+width = 40
+height = 60
+
 # Main game loop
 run = True
 while run:
@@ -21,6 +27,9 @@ while run:
         # Checking if the game has been exited
         if event.type == pygame.QUIT:
             run = False
+
+    # Displaying a rectangle
+    pygame.draw.rect(window, (255, 0, 0), (x, y, width, height))
 
 # Closes the window
 pygame.quit()
